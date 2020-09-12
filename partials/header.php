b9afa1bec84a63a0a4866586817b54480baa639a
sesp13 <sespinosarte99@gmail.com>
2020-09-12 12:30:55 -0500

    feat: fondo negro del modal de sesión

--- partials/header.php
+++ partials/header.php
@@ -26,7 +26,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Iniciar sesión</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Banco de apuestas: Iniciar sesión</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="lf-close">
                             <span aria-hidden="true">&times;</span>
                         </button>
