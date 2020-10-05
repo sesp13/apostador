ff8f60f6a9d47a6c3c9fd2b852069abb18ec3e29
sesp13 <sespinosarte99@gmail.com>
2020-10-04 21:42:54 -0500

    feat historicos de septiembre

---
+++ database/historicos/septiembre 2020/banco.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.8.4
+ */
+
+/**
+ * Database `byrvzgxbut1hzitzlhub`
+ */
+
+/* `byrvzgxbut1hzitzlhub`.`banco` */
+$banco = array(
+  array('id' => '1','valorInicial' => '500000','valorActual' => '582211','porcentaje' => '2.5')
+);
