1b3a8ecbfc745bfb8674a0e4ff444713b704d6cb
sesp13 <sespinosarte99@gmail.com>
2020-10-25 23:18:06 -0500

    feat: cambio de host

--- helpers/global-info.php
+++ helpers/global-info.php
@@ -1,4 +1,6 @@
 <?php 
 
-$HOST = "http://localhost/apostador/";
+// $HOST = "http://localhost/apostador/";
 // $HOST = "https://apostador-manage.herokuapp.com/";
+//Host del dienton
+$HOST = "https://apostador-dienton.herokuapp.com/";
