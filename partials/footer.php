8dfccbefe10c6a73d31b25ba8cebd08c101307fd
sesp13 <sespinosarte99@gmail.com>
2020-12-21 09:40:52 -0500

    importacion de la libreria sweet alert para alertas dentro de la aplicacion

--- partials/footer.php
+++ partials/footer.php
@@ -3,8 +3,9 @@
     Desarrollado por SESP13 Developments &copy; <?php echo date('Y') ?>
 </footer>
 
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/popper.js"></script>
+<script type="text/javascript" src="js/imports/jquery.js"></script>
+<script type="text/javascript" src="js/imports/popper.js"></script>
+<script type="text/javascript" src="js/imports/sweetAlert.min.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
 </body>
